f52dca1b15f8249ef8e655b67f10c196e42c30ee
Anixer1 <165531263+Anixer1@users.noreply.github.com>
2024-03-31 17:43:24 +0800

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -8,9 +8,9 @@ const dlapi = "/download/";
 // Api Server Manager
 
 const AvailableServers = [
-    "https://animedexapi.rexcalumba86.workers.dev",
-    "https://animedexapi.xeranime707.workers.dev",
-    "https://animxer-api1.xeranime707.workers.dev",
+    "https://gogoanimeapi-chi.vercel.app",
+    "https://gogoanimeapi-chi.vercel.app",
+    "https://gogoanimeapi-chi.vercel.app",
 ];
 
 function getApiServer() {
